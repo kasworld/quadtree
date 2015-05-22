fd0b154f32ffea3bdca117de671c850a14504fe0
SeukWon Kang <kasworld@gmail.com>
2015-05-22 16:33:39 +0900

    apply typed enum

--- quadtree.go
+++ quadtree.go
@@ -1,3 +1,14 @@
+// Copyright 2015 SeukWon Kang (kasworld@gmail.com)
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // for 2d space partitioning
 package quadtree
 
@@ -5,6 +16,7 @@ import (
 	"bytes"
 	"fmt"
 
+	"github.com/kasworld/idgen"
 	"github.com/kasworld/ivector2d"
 	"github.com/kasworld/rect"
 )
@@ -15,7 +27,7 @@ const (
 
 type QuadTreeObjI interface {
 	GetRect() rect.Rect
-	GetID() int64
+	GetID() idgen.IDInt
 }
 
 type QuadTree struct {
--- quadtree_test.go
+++ quadtree_test.go
@@ -1,18 +1,30 @@
+// Copyright 2015 SeukWon Kang (kasworld@gmail.com)
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package quadtree
 
 import (
 	"testing"
 
 	// "github.com/kasworld/ivector2d"
+	"github.com/kasworld/idgen"
 	"github.com/kasworld/rect"
 )
 
 type qtobj struct {
-	id int64
+	id idgen.IDInt
 	rt rect.Rect
 }
 
-func (po *qtobj) GetID() int64 {
+func (po *qtobj) GetID() idgen.IDInt {
 	return po.id
 }
 
